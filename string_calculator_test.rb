ad1e7130f53121b644cf529137c66c695ed4533c
sasikumargn <sasikumargn@gmail.com>
2025-02-25 00:09:55 +0530

    Added multiple numbers in string.

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -6,13 +6,25 @@ class StringCalculatorTest < Minitest::Test
     @calc = StringCalculator.new
   end
 
-	def test_empty_string_returns_zero
+  def test_empty_string_returns_zero
     assert_equal 0, @calc.add("")
   end
 
-	def test_single_number
+  def test_single_number
     assert_equal 1, @calc.add("1")
     assert_equal 5, @calc.add("5")
+  end
+
+  def test_two_numbers
+    assert_equal 6, @calc.add("1,5")
+  end
+
+  def test_multiple_numbers
+    assert_equal 10, @calc.add("1,2,3,4")
+  end
+
+  def test_newline_as_delimiter
+    assert_equal 6, @calc.add("1\n2,3")
   end
 
 end
