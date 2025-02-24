fa3d8661f2ee9e0d7941323ba986bb236f1deed9
sasikumargn <sasikumargn@gmail.com>
2025-02-24 23:44:02 +0530

    Initial commit for the empty string add.

---
+++ string_calculator_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require_relative 'string_calculator'
+
+class StringCalculatorTest < Minitest::Test
+  def setup
+    @calc = StringCalculator.new
+  end
+
+	def test_empty_string_returns_zero
+    assert_equal 0, @calc.add("")
+  end
+
+end
