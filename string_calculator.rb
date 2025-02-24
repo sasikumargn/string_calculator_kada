11cf857d34437768340207e4731d97c49d1703dc
sasikumargn <sasikumargn@gmail.com>
2025-02-25 00:45:20 +0530

    added more string chars to add.

--- string_calculator.rb
+++ string_calculator.rb
@@ -8,6 +8,29 @@ class StringCalculator
   end
 
   def digits(numbers)
-    numbers.split(',').map(&:to_i)
+    numbers.split(delimiter(numbers)).map(&:to_i)
   end
+
+  def delimiter(numbers)
+    if has_custom_delimiter?(numbers)
+      custom_delimiters(numbers)
+    else
+      /,|\n/
+    end
+  end
+
+  def has_custom_delimiter?(numbers)
+    numbers.start_with?("//")
+  end
+
+  def custom_delimiters(numbers)
+    delimiters = numbers.match(%r{//(\[.*?\])+\n})
+    if delimiters
+      delimiters[1].scan(/\[(.*?)\]/).flatten.map { |d| Regexp.escape(d) }.join("|")
+    else
+      delimiter_section = numbers.split("\n", 2).first
+      Regexp.escape(delimiter_section[2..]) 
+    end
+  end
+
 end
